a22bfb22c2f974c0d2740504f70dfea4cea92944
Vladislav <nicochan00@mail.u>
2022-07-06 01:22:14 +0300

    script dropdown text

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const boxes = Array.from(document.querySelectorAll('.box'));
+
+boxes.forEach(item => {
+   item.addEventListener('click', boxHandler);
+});
+
+function boxHandler(e) {
+   e.preventDefault();
+
+   let currentBox = e.target.closest('.box');
+   let currentContent = e.target.nextElementSibling;
+   currentBox.classList.toggle('active');
+
+   if (currentBox.classList.contains('active')) {
+      currentContent.style.maxHeight = currentContent.scrollHeight + 'px';
+   } else {
+      currentContent.style.maxHeight = 0;
+   }
+}
